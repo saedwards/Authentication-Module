b858c3458b71d2815630eea1f374c8694184a155
Shane Edwards <Shane Edwards>
2016-09-14 19:12:46 +0100

    Tests setup for registration

--- src/client/app/+account/registration/registration.component.spec.ts
+++ src/client/app/+account/registration/registration.component.spec.ts
@@ -10,20 +10,109 @@ import { getDOM } from '@angular/platform-browser/src/dom/dom_adapter';
 import { RegistrationComponent } from './registration.component';
 
 export function main() {
-    describe('Registration component', () => {
+
+    describe("Registration component", () => {
 
         let providerArr: any[];
 
         beforeEach(() => { providerArr = [disableDeprecatedForms(), provideForms()]});
 
         // Constructor
-        it('should initial correctly', () => {
+        it("should initialise correctly", () => {
             expect(true).toBe(true);
         });
 
-        it('should post a User object to the server', () => {
-            expect(false).toBe(false);
+        describe("register [method]", () => {
+
+            describe("when supplied valid user data", () => {
+
+                it("should post a valid NewUser type object", () => {
+                    expect(false).toBe(false);
+                });
+
+                it("should call the authentication service", () => {
+
+                });
+            });
+
+            describe("when supplied invalid user data", () => {
+
+                it("should not call the authentication service", () => {
+
+                });
+            });
+
+            describe("when server is unavailable", () => {
+
+                it("should handle post request server failures gracefully", () => {
+
+                });
+            });
+
         });
+
+        describe("validateUser [method]", () => {
+
+            it("should return false when supplied with any invalid NewUser data fields", () => {
+
+            });
+
+            it("should return true when all data fields supplied are valid", () => {
+
+            });
+        });
+
+        /**
+         * Should be imported from external file
+         */
+        describe("validateEmail [external utility]", () => {
+
+            it("should return true when identifying a correct email address", () => {
+
+            });
+
+            it("should return false when identifying an incorrect user email address", () => {
+
+            });
+        });
+
+        describe("validateUsername [external utility]", () => {
+
+            it("should return true when identifying a valid username", () => {
+
+            });
+
+            it("should return false when identifying an invalid username", () => {
+
+            });
+        });
+
+        describe("validatePassword [external utility]", () => {
+
+            it("should return false when password and confirmPassword fields don't match", () => {
+
+            });
+
+            it("should return false when password is less than 6 characters long", () => {
+
+            });
+
+            it("should return false when password does not contain at least one non-letter character", () => {
+
+            });
+
+            it("should return false when password does not contain at least one upper-case character", () => {
+
+            });
+
+            it("should return false when password does not contain at least one lower-case character", () => {
+
+            });
+        });
+
+        /**
+         * Check Taiseer's code for more validation that can be exposed on the client.
+         */
 
     });
 }
--- src/client/app/+account/registration/registration.component.ts
+++ src/client/app/+account/registration/registration.component.ts
@@ -10,6 +10,9 @@ import { NewUser } from '../../models/user';
 })
 export class RegistrationComponent {
 
+    /**
+     * Convert simple properties to RxJS observables and attach validation
+     */
     private firstName:String = '';
     private lastName:String = '';
     private email:String = '';
@@ -34,59 +37,4 @@ export class RegistrationComponent {
         return this.authenticationService.register(userModel);
 
     }
-
-    /*private post (obj) {
-        let headers = new Headers({
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'});
-
-        return this.http.post(
-            this.apiUrl,
-            JSON.stringify(obj),
-            {
-                headers: headers
-            })
-            .toPromise()
-            .then(res => res.json().data)
-            .catch(this.handlePostError.bind(this));
-    }
-
-    private validateForm () {
-
-        if (!this.firstName) {
-
-        }
-    }
-
-    private handlePostError (error: any) {
-        let errors = [],
-            currentErrorModelState;
-
-        if(error._body) {
-            currentErrorModelState = JSON.parse(error._body).modelState;
-
-            for(let i in currentErrorModelState) {
-                errors.push(...currentErrorModelState[i]);
-            }
-        }
-
-        this.errors.push(...errors);
-
-        console.log(this.errors);
-
-
-        console.log(JSON.parse(error._body));
-        console.log('-----');
-
-        let errMsg = (error.message) ? error.message :
-            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-
-        console.error(errMsg); // log to console instead
-
-        /!*if(error instanceof Response) {
-            return Observable.throw(error.json().error || 'backend server error');
-        }
-
-        return Observable.throw(errMsg);*!/
-    }*/
 }
